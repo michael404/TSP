ac3a9681bc96d333c6129a83f141e40ec622d1b7
Michael <michael404@gmail.com>
2018-06-25 22:28:42 +0200

    Initial Commit

---
+++ Draw/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Draw
+//
+//  Created by Michael Holmgren on 2018-06-25.
+//  Copyright © 2018 Michael Holmgren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Draw/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Draw
+//
+//  Created by Michael Holmgren on 2018-06-25.
+//  Copyright © 2018 Michael Holmgren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
