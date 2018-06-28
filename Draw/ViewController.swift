b9cdb341946f2e114054906af7d499d7464e9f51
Michael <michael404@gmail.com>
2018-06-28 21:39:18 +0200

    Updated viewport size

--- Draw/ViewController.swift
+++ Draw/ViewController.swift
@@ -4,13 +4,13 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let frame = CGRect(x: 0, y: 0, width: 480, height: 480)
+        let frame = CGRect(x: 0, y: 0, width: 800, height: 800)
         
         let data = readData(from: "sweden"); var route = Route(nearestNeighborFrom: data, startAt: 0)
-//        var route = Route(nearestNeighborFrom: zimbabwe, startAt: 0)
+        // var route = Route(nearestNeighborFrom: zimbabwe, startAt: 0)
         route.opt2()
         
-        view.addSubview(DrawView(frame: frame, points: route.export(max: 480)))
+        view.addSubview(DrawView(frame: frame, points: route.export(max: 800)))
         
     }
 
