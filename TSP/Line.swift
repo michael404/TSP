83e029b5c47255473a9f056d7c35509bdcc0d0f4
Michael <michael404@gmail.com>
2018-07-08 11:32:11 +0200

    Got some nice 10-20% speed improvements by changing Point to use a simd_float2 and using the specialised simd distance implementations

--- TSP/Line.swift
+++ TSP/Line.swift
@@ -1,24 +1,24 @@
+import simd
+
 struct Line {
-    let p1, p2: Point
+    let p1, p2: simd_float2
     
     init(_ p1: Point, _ p2: Point) {
-        self.p1 = p1
-        self.p2 = p2
+        self.p1 = p1._point
+        self.p2 = p2._point
     }
     
     var distance: Float {
         @inline(__always)
         get {
-            return distanceSquared.squareRoot()
+            return simd_distance(p1, p2)
         }
     }
     
     var distanceSquared: Float {
         @inline(__always)
         get {
-            let distanceX = p2.x - p1.x
-            let distanceY = p2.y - p1.y
-            return (distanceX * distanceX + distanceY * distanceY)
+            return simd_distance_squared(p1, p2)
         }
     }
 }
--- TSP/Point.swift
+++ TSP/Point.swift
@@ -1,19 +1,29 @@
-struct Point: Hashable {
+import simd
+
+struct Point: Equatable {
     
-    let x: Float
-    let y: Float
+    let _point: simd_float2
+    
+    var x: Float { return _point.x }
+    var y: Float { return _point.y }
     
     init(_ x: Float, _ y: Float) {
-        self.x = x
-        self.y = y
+        self._point = simd_make_float2(x, y)
     }
     
 }
 
 extension Point: CustomStringConvertible {
     var description: String {
-        return "(\(x), \(y))"
+        return "(\(_point.x), \(_point.y))"
     }
     
     
+}
+
+extension Point: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(_point.x)
+        hasher.combine(_point.y)
+    }
 }
